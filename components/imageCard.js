f93ce0f307f6667755263a7eb202b2707f2c5748
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-07-03 17:09:30 +0530

    Image Grid

---
+++ components/imageCard.js
@@ -0,0 +1,42 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import { getImageSize } from "../helpers/common";
+import { theme } from "../constants/theme";
+
+const ImageCard = ({ item, index }) => {
+  const getImageHeight = () => {
+    let { imageHeight: height, imageWidth: width } = item;
+    return { height: getImageSize(height, width) };
+  };
+  const isLastrow = () => {
+    return (index + 1) % columns === 0;
+  };
+  return (
+    <Pressable style={[styles.imageWrapper, !isLastrow() && styles.spacing]}>
+      <Image
+        style={[styles.image, getImageHeight()]}
+        source={item?.webformatURL}
+        transition={100}
+      />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    height: 300,
+    width: "100%",
+  },
+  imageWrapper: {
+    backgroundColor: theme.colors.grayBG,
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+    overflow: "hidden",
+    marginBottom: wp(2),
+  },
+  spacing: {
+    marginRight: wp(2),
+  },
+});
+
+export default ImageCard;
--- components/imageGrid.js
+++ components/imageGrid.js
@@ -1,13 +1,34 @@
 import { View, Text } from "react-native";
+import { MasonryFlashList } from "@shopify/flash-list";
+import ImageCard from "./imageCard";
+import { getColumnCount } from "../helpers/common";
 
-const ImageGrid = () => {
+const ImageGrid = ({ images }) => {
+  const columns = getColumnCount();
   return (
-    <View>
-      <Text>imageGrid</Text>
+    <View styles={styles.container}>
+      <MasonryFlashList
+        data={images}
+        numColumns={columns}
+        renderItem={({ item }) => (
+          <ImageCard item={item} index={index} columns={columns} />
+        )}
+        estimatedItemSize={200}
+        contentContainerStyle={styles.listContainerStyle}
+        initialNumberToRender={1000}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    minHeight: 3,
+    width: wp(100),
+  },
+  listContainerStyle: {
+    paddingHorizontal: wp(4),
+  },
+});
 
 export default ImageGrid;
